25e6710da936915d923cea0262304ec0ec111ef4
burns <burnst96@gmail.com>
2020-06-21 17:52:11 +0100

    Added dev/prod profiles + mysql db support
    (cherry picked from commit ea114f1c0cb84ee78d39b3e3f09914abdeb018c0)

--- src/main/java/com/slidestream/MainApplication.java
+++ src/main/java/com/slidestream/MainApplication.java
@@ -2,15 +2,19 @@ package com.slidestream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:application-${spring.profiles.active}.properties")
+})
 public class MainApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-
 }
 
